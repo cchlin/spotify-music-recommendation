9990d1d45ed8e314573bd9d9290212dfee73acd0
Cheng Lin <chlin@pdx.edu>
2023-06-16 07:40:40 +0000

    entry page basic stuff done

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask.views import MethodView
 from index import Index
 from callback import Callback
 from logout import Logout
+from select import Select
 
 app = flask.Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -11,6 +12,8 @@ app.secret_key = os.urandom(24)
 app.add_url_rule('/', view_func=Index.as_view('index'), methods=['GET'])
 
 app.add_url_rule('/callback', view_func=Callback.as_view('callback'), methods=['GET'])
+
+app.add_url_rule('/select', view_func=Select.as_view('select'), methods=['GET'])
 
 app.add_url_rule('/logout', view_func=Logout.as_view('logout'), methods=['GET'])
 
