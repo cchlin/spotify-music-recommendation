5427fe8b2fb37be45d42ac2be2283e63e65e71b5
cchlin <101103676+cchlin@users.noreply.github.com>
2023-09-08 03:21:32 -0700

    fixed create_playlist not working problem; added a modal to let user know the list is created and redirect back to the search page

--- create_playlist.py
+++ create_playlist.py
@@ -6,22 +6,24 @@ class CreatePlaylist(MethodView):
 	def post(self):
 		if 'token' in session:
 			try:
-				sp.token = session['token']
-				sp_with_token = spotipy.Spotify(auth=access_token)
+				spToken = session['token']
+				user = sp.current_user()
 				user_id = user['id']
 				playlist_name = "New Playlist"
 				playlist_description = "Created with Spotify recommendation"
-				playlist = sp_with_token.user_playlist_create(user_id, playlist_name, public=True, collaborative=False, description=playlist_description)
+				playlist = sp.user_playlist_create(user_id, playlist_name, public=True, collaborative=False, description=playlist_description)
 				# Retrieve track URIs from session
 				song_uris = ['spotify:track:' + track['id'] for track in session.get('tracks', [])]
 				if song_uris:
-					results = sp_with_token.playlist_add_items(playlist['id'], song_uris);
+					results = sp.playlist_add_items(playlist['id'], song_uris);
 				else:
 					return jsonify({'status': 'error', 'message': 'No songs to add to playlist'}), 400
-					return jsonify({'status': 'success', 'playlist': playlist}), 200
+				return jsonify({'status': 'success', 'playlist': playlist}), 200
 			except Exception as e:
+				print("Create playlist try except e", e)
 				return jsonify({'status': 'error', 'message': str(e)}), 500
 		else:
 			return jsonify({'status': 'error', 'message': 'Not logged in'}), 401
+
 
 
